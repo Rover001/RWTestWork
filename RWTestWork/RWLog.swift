6503e6398a73ac4f5ee0c4a246c7aa4454fc3e83
Rover001 <zengyun6666@163.com>
2019-12-07 10:03:04 +0800

    测试Swift Carthger使用

---
+++ RWTestWork/RWLog.swift
@@ -0,0 +1,16 @@
+//
+//  RWLog.swift
+//  RWTestWork
+//
+//  Created by linmao on 2019/11/28.
+//  Copyright © 2019 RoverWord. All rights reserved.
+//
+
+import UIKit
+
+public class RWLog: NSObject {
+    
+    public func customLog() ->String {
+        return "RWLog-->customLog"
+    }
+}
---
+++ RWTestWorkDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RWTestWorkDemo
+//
+//  Created by linmao on 2019/11/28.
+//  Copyright © 2019 RoverWord. All rights reserved.
+//
+
+import UIKit
+import RWTestWork
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(RWLog.init().customLog())
+    }
+
+
+}
+
